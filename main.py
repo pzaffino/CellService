32bff3687fb10381259beaae25a227b8287c2441
Paolo Zaffino <p.zaffino@yahoo.it>
2020-12-31 19:26:27 +0100

    Migrate from Pyside2 to PyQt5 and add open menu

--- main.py
+++ main.py
@@ -2,24 +2,39 @@
 import sys
 import os
 
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QFileDialog
+from PyQt5.QtCore import QFile
+from PyQt5.QtGui import QIcon
+import PyQt5.uic
 
-from PySide2.QtWidgets import QApplication, QWidget
-from PySide2.QtCore import QFile
-from PySide2.QtUiTools import QUiLoader
-
-
-class CellService(QWidget):
+class CellService(QMainWindow):
     def __init__(self):
         super(CellService, self).__init__()
         self.load_ui()
+        self.create_menu()
 
     def load_ui(self):
-        loader = QUiLoader()
         path = os.path.join(os.path.dirname(__file__), "form.ui")
         ui_file = QFile(path)
         ui_file.open(QFile.ReadOnly)
-        loader.load(ui_file, self)
+        loader = PyQt5.uic.loadUi(ui_file)
         ui_file.close()
+
+    def create_menu(self):
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu("File")
+
+        openAction = QAction(QIcon('open.png'), '&Open', self)
+        openAction.setShortcut('Ctrl+O')
+        openAction.setStatusTip('Open a new image')
+        openAction.triggered.connect(self.openCall)
+        fileMenu.addAction(openAction)
+
+    def openCall(self):
+        print('Open')
+        file_name = QFileDialog.getOpenFileName(self, 'Open File')
+        if file_name:
+            print(file_name)
 
 if __name__ == "__main__":
     app = QApplication([])
