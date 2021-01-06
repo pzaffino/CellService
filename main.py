54e4dc70dbfd352b59149e3e78949ef544ee9956
Paolo Zaffino <p.zaffino@yahoo.it>
2021-01-06 21:52:52 +0100

    Now image is read by skimage (preliminary version)

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import os
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QFileDialog
 from PyQt5.QtCore import QFile
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtGui import QIcon, QImage, QPixmap, qRgb
 import PyQt5.uic
+
+import numpy as np
+import skimage.io
 
 class CellService(QMainWindow):
     def __init__(self):
@@ -31,12 +34,16 @@ class CellService(QMainWindow):
         fileMenu.addAction(openAction)
 
     def openCall(self):
+        # see https://gist.github.com/smex/5287589
         print('Open RGB Image')
         file_name = QFileDialog.getOpenFileName(self, 'Open RGB Image')
+
         if file_name:
-            RGB_pixmap = QPixmap(file_name[0])
-            self.RGB_QLabel.setPixmap(RGB_pixmap)
-            print(file_name)
+            print(file_name[0])
+            self.RGB_image = skimage.io.imread(file_name[0]).astype(np.uint8)
+
+            RGB_QImage = QImage(self.RGB_image.data, self.RGB_image.shape[1], self.RGB_image.shape[0], self.RGB_image.strides[0], QImage.Format_RGB888)
+            self.RGB_QLabel.setPixmap(QPixmap.fromImage(RGB_QImage))
 
 if __name__ == "__main__":
     app = QApplication([])
