cf03fd1d4997e6006ef88cf42a882e26dc5f38dd
Paolo Zaffino <p.zaffino@yahoo.it>
2021-03-09 17:25:34 +0100

    Now it opens images

--- CellService.py
+++ CellService.py
@@ -2,7 +2,11 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QLabel, QAction, QFileDialog, QMenuBar, QMainWindow
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtGui import QIcon, QPixmap, QImage, qRgb
+
+import numpy as np
+import skimage.io
+
 
 class CellService(QMainWindow):
 
@@ -23,63 +27,80 @@ class CellService(QMainWindow):
         self.setWindowTitle("CellService")
         self.create_menu()
 
-        pixmap = QPixmap('/home/p4ol0/Pictures/logo_vv.png')
+        init_size = 512
 
         # RGB
-        self.QLabelRGB = QLabel(self)
-        self.QLabelRGB.setStyleSheet("border: 2px solid black;")
-        self.QLabelRGB.setScaledContents(True)
-        grid.addWidget(self.QLabelRGB, 0,0)
-        self.QLabelRGB.setPixmap(pixmap)
+        self.RGB_QLabel = QLabel(self)
+        self.RGB_QLabel.setStyleSheet("border: 2px solid black;")
+        self.RGB_QLabel.setScaledContents(True)
+        grid.addWidget(self.RGB_QLabel, 0,0)
+        self.RGB_QLabel.resize(init_size, init_size)
 
         # RED
-        self.QLabelR = QLabel(self)
-        self.QLabelR.setStyleSheet("border: 2px solid red;")
-        self.QLabelR.setScaledContents(True)
-        grid.addWidget(self.QLabelR, 0,1)
-        self.QLabelR.setPixmap(pixmap)
+        self.Red_QLabel = QLabel(self)
+        self.Red_QLabel.setStyleSheet("border: 2px solid red;")
+        self.Red_QLabel.setScaledContents(True)
+        grid.addWidget(self.Red_QLabel, 0,1)
+        self.Red_QLabel.resize(init_size, init_size)
 
         # GREEN
-        self.QLabelG = QLabel(self)
-        self.QLabelG.setStyleSheet("border: 2px solid green;")
-        self.QLabelG.setScaledContents(True)
-        grid.addWidget(self.QLabelG, 1,0)
-        self.QLabelG.setPixmap(pixmap)
+        self.Green_QLabel = QLabel(self)
+        self.Green_QLabel.setStyleSheet("border: 2px solid green;")
+        self.Green_QLabel.setScaledContents(True)
+        grid.addWidget(self.Green_QLabel, 1,0)
+        self.Green_QLabel.resize(init_size, init_size)
 
         # BLUE
-        self.QLabelB = QLabel(self)
-        self.QLabelB.setStyleSheet("border: 2px solid blue;")
-        self.QLabelB.setScaledContents(True)
-        grid.addWidget(self.QLabelB, 1,1)
-        self.QLabelB.setPixmap(pixmap)
+        self.Blue_QLabel = QLabel(self)
+        self.Blue_QLabel.setStyleSheet("border: 2px solid blue;")
+        self.Blue_QLabel.setScaledContents(True)
+        grid.addWidget(self.Blue_QLabel, 1,1)
+        self.Blue_QLabel.resize(init_size, init_size)
 
         # Show the window
+        self.resize(1024,1024)
         self.show()
 
     def create_menu(self):
-        #mainMenu = QMenuBar(self)
         mainMenu = self.menuBar()
         fileMenu = mainMenu.addMenu("File")
 
-        openAction = QAction(QIcon('open.png'), '&Open', self)
+        openAction = QAction(QIcon('open.png'), '&Open RGB', self)
         openAction.setShortcut('Ctrl+O')
-        openAction.setStatusTip('Open a new image')
+        openAction.setStatusTip('Open a new RGB image')
         openAction.triggered.connect(self.openCall)
         fileMenu.addAction(openAction)
+
+    def set_image(self, np_array, qt_label, channel):
+
+        if channel == "rgb":
+            image = np_array
+        elif channel == "red":
+            image = np.zeros((np_array.shape[0], np_array.shape[1], 3), dtype=np.uint8)
+            image[:,:,0] = np_array
+        elif channel == "green":
+            image = np.zeros((np_array.shape[0], np_array.shape[1], 3), dtype=np.uint8)
+            image[:,:,1] = np_array
+        elif channel == "blue":
+            image = np.zeros((np_array.shape[0], np_array.shape[1], 3), dtype=np.uint8)
+            image[:,:,2] = np_array
+
+        qt_image = QImage(image.data, image.shape[1], image.shape[0], image.strides[0], QImage.Format_RGB888)
+        qt_label.setPixmap(QPixmap.fromImage(qt_image))
 
     def openCall(self):
         # see https://gist.github.com/smex/5287589
         print('Open RGB Image')
         file_name = QFileDialog.getOpenFileName(self, 'Open RGB Image')
 
-        """
+
         if file_name:
             self.RGB_image = skimage.io.imread(file_name[0]).astype(np.uint8)
             self.set_image(self.RGB_image, self.RGB_QLabel, "rgb")
             self.set_image(self.RGB_image[:,:,0], self.Red_QLabel, "red")
             self.set_image(self.RGB_image[:,:,1], self.Green_QLabel, "green")
             self.set_image(self.RGB_image[:,:,2], self.Blue_QLabel, "blue")
-        """
+
 
 if __name__ == '__main__':
      app = QApplication(sys.argv)
