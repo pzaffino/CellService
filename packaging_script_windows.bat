94c04a5c76b955b03017d53691314ed6ba52414f
Paolo Zaffino <p.zaffino@yahoo.it>
2021-05-16 22:50:19 +0200

    Add pyinstaller script

---
+++ packaging_script_windows.bat
@@ -0,0 +1,1 @@
+pyinstaller --onefile --hidden-import PyQt5.sip --hidden-import PyQt5.QtWidgets --hidden-import CellService_processing --hidden-import skimage.filters.rank.core_cy_3d --hidden-import Analisys_cellService CellService.py
